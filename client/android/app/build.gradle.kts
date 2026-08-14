309b5b728b885ee1fa09a180178f2bbc98607668
executor <executor@local>
2026-08-14 10:47:31 +0100

    Group 5 prep: Android build fix (file_picker 12 + secure_storage 11 + compileSdk 37) + docs; APK built, walkthrough awaits device

--- client/android/app/build.gradle.kts
+++ client/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.spotifyclone.spotify_clone"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
